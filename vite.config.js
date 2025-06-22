6ffa00c9914e99ae29974b8bb24d90ced89e1af7
Chrispoo3680 <christobran@gmail.com>
2025-06-22 19:42:31 +0200

    Small fix

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/fitlinehealth.fit",
   resolve: {
     alias: {
-      "@components": path.resolve(__dirname, 'src/components'),
-      "@pages": path.resolve(__dirname, 'src/pages'),
-      "@assets": path.resolve(__dirname, 'src/assets'),
-      "@styles": path.resolve(__dirname, 'src/styles'),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@styles": path.resolve(__dirname, "src/styles"),
     },
   },
-})
+});
